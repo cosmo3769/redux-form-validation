c62c73fe5b10c4903bbfb0a342f8f584086f8635
cosmo3769 <piyushthakur3769@gmail.com>
2022-07-25 23:25:10 +0530

    update validation

--- src/components/validators.js
+++ src/components/validators.js
@@ -7,6 +7,18 @@ export const required = (value) => {
 
 export const name = value => {
     if (value && !/^[a-z ,.'-]+$/i.test(value)) {
-        return 'Invalid name'
+        return 'Invalid name!'
     }
 };
+
+export const maxLength = value => {
+  if (value && value.length>20) {
+    return 'Must be 20 characters or less!'
+  }
+}
+
+export const tinorssn = value => {
+  if (value && /^(?:\d{3}-\d{2}-\d{4}|\d{2}-\d{7}|^[1-9][0-9]*$)$/) {
+      return 'Invalid tin/ssn!'
+  }
+}
