4ff53a87ad5a7708c9b91d07c20158987621dfad
cosmo3769 <piyushthakur3769@gmail.com>
2022-07-26 18:32:32 +0530

    added country field

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import countryList from './countries'
 import Input from './input'
 import Select from './select'
 import { maxLength, name, required, tinorssn } from './validators'
 
 let FormComponent = (props) => {
-  const { handleSubmit } = props
+  const { handleSubmit, formValues } = props
   return (
     <form onSubmit={handleSubmit}>
       <Field 
@@ -23,6 +24,15 @@ let FormComponent = (props) => {
       <Field name="middleName" type="text" component={Input} label="Middle Name" validate={[name, maxLength]}/>
       <Field name="lastName" type="text" component={Input} label="LastName*" validate={[required, name, maxLength]}/>
       <Field name="tin" type="number" component={Input} label="Tin/Ssn*" validate={[required, tinorssn]}/>
+      <Field 
+        name="country" 
+        component={Select}
+        options={countryList}
+        label="Country*"
+        validate={required} />
+      {/* {formValues && formValues.country === 'Ukraine' ? (
+          <Field name="middleName" type="text" component={Input} label="Middle Name" validate={[name, maxLength]}/>
+        ) : ''} */}
       <button type="submit">Submit</button>
     </form>
   )
