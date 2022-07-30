31e44e456d33b2d843cf6c017ee32b97f674f883
cosmo3769 <piyushthakur3769@gmail.com>
2022-07-31 01:10:46 +0530

    added progress-bar

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -3,6 +3,7 @@ import FormComponentFirst from './FormComponentFirst'
 import FormComponentSecond from './FormComponentSecond'
 import FormComponentThird from './FormComponentThird'
 import PropTypes from 'prop-types'
+import MultiStepProgressBar from './MultiPageProgressBar'
 
 class FormComponent extends Component {
   constructor(props) {
@@ -10,24 +11,82 @@ class FormComponent extends Component {
     this.nextPage = this.nextPage.bind(this)
     this.previousPage = this.previousPage.bind(this)
     this.state = {
-      page: 1
+      currentStep: 1
     }
   }
+  
   nextPage() {
-    this.setState({ page: this.state.page + 1 })
+    let currentStep = this.state.currentStep;
+
+    // If the current step is 1 or 2, then add one on "next" button click
+    currentStep = currentStep >= 2 ? 3 : currentStep + 1;
+    this.setState({
+      currentStep: currentStep
+    });
   }
 
   previousPage() {
-    this.setState({ page: this.state.page - 1 })
+    let currentStep = this.state.currentStep;
+    // If the current step is 2 or 3, then subtract one on "previous" button click
+    currentStep = currentStep <= 1 ? 1 : currentStep - 1;
+    this.setState({
+      currentStep: currentStep
+    });
+  }
+
+  // The "next" and "previous" button functions
+  get previousButton() {
+    let currentStep = this.state.currentStep;
+
+    // If the current step is not 1, then render the "previous" button
+    if (currentStep !== 1) {
+      return (
+        <button className='previous' onClick={this.previousPage}>
+          Previous
+        </button>
+      );
+    }
+
+    // ...else return nothing
+    return null;
+  }
+
+  get nextButton() {
+    let currentStep = this.state.currentStep;
+    // If the current step is not 3, then render the "next" button
+    if (currentStep < 3) {
+      return (
+        <button color="primary float-right" onClick={this.nextPage}>
+          Next
+        </button>
+      );
+    }
+    // ...else render nothing
+    return null;
+  }
+
+  get submitButton() {
+    let currentStep = this.state.currentStep;
+
+    // If the current step is the last step, then render the "submit" button
+    if (currentStep > 2) {
+      return <button>Submit</button>;
+    }
+    // ...else render nothing
+    return null;
   }
 
   render() {
     const { onSubmit } = this.props
     const { page } = this.state
     return (<div>
-        {page === 1 && <FormComponentFirst onSubmit={this.nextPage}/>}
-        {page === 2 && <FormComponentSecond previousPage={this.previousPage} onSubmit={this.nextPage}/>}
-        {page === 3 && <FormComponentThird previousPage={this.previousPage} onSubmit={onSubmit}/>}
+        <MultiStepProgressBar currentStep={this.state.currentStep} />
+        <FormComponentFirst currentStep={this.state.currentStep} />
+        <FormComponentSecond currentStep={this.state.currentStep} />
+        <FormComponentThird currentStep={this.state.currentStep} />
+        {this.previousButton}
+        {this.nextButton}
+        {this.submitButton}
       </div>
     )
   }
--- src/components/FormComponentSecond.jsx
+++ src/components/FormComponentSecond.jsx
@@ -1,41 +1,17 @@
 import React from 'react'
-import { Field, formValueSelector, reduxForm } from 'redux-form'
-import { connect } from 'react-redux'
-import countryList from './countries'
+import { Field, reduxForm } from 'redux-form'
 import Input from './input'
-import Select from './select'
 import { maxLength, name, required, tinorssn } from './validators'
 
 let FormComponentSecond = (props) => {
-  const { handleSubmit, previousPage, country } = props
+  const { handleSubmit } = props
+  if (props.currentStep !== 2) {
+    return null;
+  }
   return (
     <form onSubmit={handleSubmit}>
-      {/* <Field 
-        name="prefix" 
-        component={Select}
-        options={{
-          Mr: 'Mr.',
-          Mrs: 'Mrs.',
-          Miss: 'Miss.',
-          Dr: 'Dr.'
-        }}
-        label="Prefix*"
-        validate={required} />
-      <Field name="firstName" type="text" component={Input} label="First Name*" validate={[required, name, maxLength]} /> */}
       <Field name="middleName" type="text" component={Input} label="Middle Name" validate={[name, maxLength]}/>
       <Field name="lastName" type="text" component={Input} label="LastName*" validate={[required, name, maxLength]}/>
-      {/* <Field name="tin" type="number" component={Input} label="Tin/Ssn*" validate={[required, tinorssn]}/>
-      <Field 
-        name="country" 
-        component={Select}
-        options={countryList}
-        label="Country*"
-        validate={required} />
-      {country !== 'US' &&
-          <Field name="middleName" type="text" component={Input} label="Middle Name" validate={[name, maxLength]}/>
-      } */}
-      <button type="button" className="previous" onClick={previousPage}>Previous</button>
-      <button type="submit" className="next">Next</button>
     </form>
   )
 }
@@ -46,15 +22,5 @@ FormComponentSecond = reduxForm({
   destroyOnUnmount: false,
   forceUnregisterOnUnmount: true,
 })(FormComponentSecond)
-
-// const selector = formValueSelector('contact')
-
-// FormComponentSecond = connect(state => {
-//   const country = selector(state, 'country')
-
-//   return {
-//     country
-//   }
-// })(FormComponentSecond)
 
 export default FormComponentSecond
